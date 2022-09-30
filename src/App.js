85aa4f4fb4ad52180afed37be190531fbf5529fb
CodeSocrates <furo9899@naver.com>
2022-09-30 21:25:56 +0900

    chore: 라우팅 및 페이지 파일 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div></div>
+    <BrowserRouter basename="secondary_shop">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:productId" element={<ProductDetail />} />
+        <Route path="/basket" element={<Basket />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
