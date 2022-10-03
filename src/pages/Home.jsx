12a91ae220f8c49918a98a32b2267c2f883710a7
CodeSocrates <furo9899@naver.com>
2022-10-04 01:43:35 +0900

    chore: 홈페이지 중앙정렬

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ import styled from 'styled-components';
 const Home = () => {
   return (
   //홈페이지 ( MainPage )
-  <div>
+  <HomeStyled>
 
     <div>
       <Navigation />
@@ -38,9 +38,15 @@ const Home = () => {
       />
     </div>
 
-  </div>
+  </HomeStyled>
   );
 };
+
+const HomeStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 const ThemeSection = styled.div`
   display:flex;
